bf46402a4dbee41a2cd9f6921ef12bb9fd045c6b
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-05-17 19:11:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const nextButton = document.querySelector('.next-btn');
 
 const video = document.querySelector('.hero-video');
 
-const gamelist = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4']
+const gamelist = ['videos H/hero-1.mp4', 'videos H/hero-2.mp4', 'videos H/hero-3.mp4', 'videos H/hero-4.mp4']
 
 let index = 0;
 nextButton.addEventListener('click', () => {
